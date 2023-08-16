2cde1d7a603742d19f2f37c3685e0be5b809caca
JamalDunn23 <jamaldunn23@gmail.com>
2023-08-16 17:29:08 -0400

    Added Bones Of Dice Rolled

--- @4/src/App.jsx
+++ @4/src/App.jsx
@@ -27,11 +27,35 @@ function App() {
   
   const player1Turn = true;
 
+  const rollDiceOnTap = () => {
+    let randomNumber = Math.floor(Math.random()* 6)+1; 
+
+    switch(randomNumber){
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+
+      default:
+
+        break;
+
+    }
+  }
+
   return (
     <>
       <div>
         <header>
-    <h1>Color Battle ?</h1>
+    <h1>Color Battle !</h1>
 
         </header>
       </div>
